974364edd5b8e331cfdddc7d00304289ec81d432
Hector808 <h.rojas.bravo@gmail.com>
2022-11-29 16:57:25 +0000

    Implemented first exercise -- capitalize

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -3,8 +3,6 @@
 export function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
    return word.substring(0,1).toUpperCase() + word.substring(1,word.length);
-   // HOLA
-   //Chao
   
 }
 
