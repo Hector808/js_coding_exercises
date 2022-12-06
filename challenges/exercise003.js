d2e6c01690ad7511c27600ee83c78181185e5745
Hector808 <h.rojas.bravo@gmail.com>
2022-12-06 01:54:52 +0000

    Implemented third full

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,3 +1,4 @@
+
 export function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
 
@@ -21,17 +22,37 @@ return words.join("");
 
 export function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+
+  let cont = 0 
+  people = people.map(element => {
+   cont = cont + element.subjects.length; 
+ })
+ return (cont);
+
 }
 
 export function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
-  // Your code here!
+
+  let arr = menu.map (toppi =>
+    toppi.ingredients.includes(ingredient)? true:false);
+    return arr.includes(true)? true : false;
+
 }
 
 export function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
-  // Your code here!
+
+
+  let newArr = arr1.map(item => arr2.includes(item) ? item : "")
+  let resArr = newArr.filter(Boolean).sort()
+  let orgArr = resArr.sort();
+  
+  const setti =new Set (orgArr);
+  return( [... setti]);
+
 }
+
+
